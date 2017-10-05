d8a28d4083baf301a6e7303684911b5fa5a08416
tier5 <work@tier5.us>
2017-10-05 14:15:05 +0530

    Fix MP4 video issue

--- application/views/user/video_gallery_view.php
+++ application/views/user/video_gallery_view.php
@@ -2,6 +2,15 @@
 <html>
     <head>
         <?php include('include/headsection.php'); ?>
+        <link href="http://vjs.zencdn.net/6.2.7/video-js.css" rel="stylesheet">
+        <style>
+            .vjs-icon-placeholder {
+                top: 10px;
+            }
+        </style>
+        <!--[if lt IE 9]>
+            <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+        <![endif]-->
     </head>
 
     <body>
@@ -25,17 +34,22 @@
                         <li class="grid_4 isotope-item photography">
                             <?php /*?><iframe width="100%" height="100%" src="https://www.youtube.com/embed/Ctugo5EJuns" frameborder="0" allowfullscreen></iframe><?php */?>
                             <figure class="portfolio-img-container">
-                                <div class="portfolio-img">
+                                <div class="portfolio-img" style="margin-bottom: 30px;">
                                 <?php if(isset($gallerylistdata['gvideo_url']) && ($gallerylistdata['video_type']=='1') ){ ?>
                                     <?php $url=(explode("?v=",$gallerylistdata['gvideo_url']));  ?>
-                                        <iframe width="100%" height="280" src="https://www.youtube.com/embed/<?php echo $url[1]; ?>" frameborder="0" allowfullscreen></iframe>
+                                        <iframe width="100%" height="260" src="https://www.youtube.com/embed/<?php echo $url[1]; ?>" frameborder="0" allowfullscreen></iframe>
                                     <?php } ?>
 
                                     <?php if(isset($gallerylistdata['gvideo_url']) && ($gallerylistdata['video_type']=='2') ){ ?>
                                         <?php  $ext=substr($gallerylistdata['gvideo_url'], strrpos($gallerylistdata['gvideo_url'], '.') + 1);?>
-                                        <video width="100%" height="280" name="media" controls>
-                                            <source src="<?php echo BASE_URI?>uploads/video/<?php echo $gallerylistdata['gvideo_url'];?>" type="video/<?php echo $ext; ?>">
-                                        </video>
+                                        <video id="my-video" class="video-js vjs-default-skin vjs-big-play-centered" controls preload="auto" width="" height="260" data-setup="{}" style="width: 100%">
+                                            <source src="<?= BASE_URI ?>uploads/video/<?= $gallerylistdata['gvideo_url'];?>" type='video/<?php echo $ext; ?>'>
+                                            <p class="vjs-no-js">
+                                              To view this video please enable JavaScript, and consider upgrading to a web browser that
+                                              <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                                            </p>
+                                          </video>
+
                                     <?php } ?>
                                 </div>
                                 <figcaption>
@@ -84,13 +98,6 @@
 
         <!-- .footer-wrapper start -->
          <?php include('include/footer.php'); ?><!-- .footer-wrapper end -->
-        
-        
-      
-         
-		 
-	
-        
-        
+        <script src="http://vjs.zencdn.net/6.2.7/video.js"></script>
     </body>
 </html>
